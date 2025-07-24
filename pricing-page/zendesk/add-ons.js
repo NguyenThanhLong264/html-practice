96a0eb699fa76fa6838064fe654350daa12bd398
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-07-24 14:14:06 +0700

    li modify

--- pricing-page/zendesk/add-ons.js
+++ pricing-page/zendesk/add-ons.js
@@ -57,7 +57,22 @@ container.innerHTML = add_on_data.map(item => `
             </div>
             <div class="add-on-li list-features">
                 <ul>
-                    ${item.features.map(f => `<li>${f}</li>`).join('')}
+                    ${item.features.map(f => `
+                        <li class='openBtn-keys'>
+      <div class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 21 16" fill="currentColor">
+          <path d="M21.053 1.758 19.295 0 6.771 12.52 1.764 7.513 0 9.277 6.771 16z"></path>
+        </svg>
+      </div>
+      <div class="key-text">
+          <span>
+            <span class='key-label no-dec'>${f}</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" class="key-icon-end">
+              <path d="m0 14.098 1.509 1.509 3.771-3.772-1.508-1.508zM9.741 0a5.867 5.867 0 1 0 0 11.735A5.867 5.867 0 0 0 9.742 0m0 1.067c2.647 0 4.8 2.154 4.8 4.8s-2.153 4.8-4.8 4.8-4.8-2.154-4.8-4.8 2.153-4.8 4.8-4.8"></path>
+            </svg>
+          </span>
+      </div>
+    </li>`).join('')}
                 </ul>
             </div>
             <div class="description">${item.bottomText}</div>
--- pricing-page/zendesk/feature-cards.js
+++ pricing-page/zendesk/feature-cards.js
@@ -9,7 +9,7 @@ const features = [
         key_feature: 'Key feature',
         keys: [
             { title: 'Ticket - Case management', description: 'Nothing' },
-            { title: 'Tính năng Email', description: 'Nothing' },
+            { title: 'Tính năng Email', description: 'Nothing', tag: 'New', },
             { title: 'Tính năng SMS', description: 'Nothing' },
             { title: 'Tính năng tổng đài', description: 'Nothing' },
             { title: 'Tính năng outbound', description: 'Nothing' },
@@ -31,7 +31,8 @@ const features = [
         keys: [
             {
                 title: 'Ticket - Case management',
-                description: 'Nothing'
+                description: 'Nothing',
+                tag: "New",
             }
         ],
         marked: true
@@ -87,8 +88,30 @@ features.forEach(item => {
                         </div>`)
 
     const listItems = item.keys.map(key => {
-        return `<li class='openBtn-keys'><span>${key.title}</span></li>`;
+        return `
+    <li class='openBtn-keys'>
+      <div class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 21 16" fill="currentColor">
+          <path d="M21.053 1.758 19.295 0 6.771 12.52 1.764 7.513 0 9.277 6.771 16z"></path>
+        </svg>
+      </div>
+      <div class="key-text">
+          <span>
+            <span class='key-label'>${key.title}</span>
+            ${key.tag ? `
+              <span class="new">
+                <p>${key.tag}</p>
+              </span>` : ''}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" class="key-icon-end">
+              <path d="m0 14.098 1.509 1.509 3.771-3.772-1.508-1.508zM9.741 0a5.867 5.867 0 1 0 0 11.735A5.867 5.867 0 0 0 9.742 0m0 1.067c2.647 0 4.8 2.154 4.8 4.8s-2.153 4.8-4.8 4.8-4.8-2.154-4.8-4.8 2.153-4.8 4.8-4.8"></path>
+            </svg>
+          </span>
+      </div>
+    </li>
+  `;
     }).join('');
+
+
     const card_features = createElementFromHTML(`<div class="card-features">
                             <p class="title-med">${item.key_feature}</p>
                             <div class="list-features">
