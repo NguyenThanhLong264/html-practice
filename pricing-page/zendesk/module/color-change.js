ae5188b96246e986ef15f7b27b4b43c59a5bd3e4
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-07-29 16:33:44 +0700

    .

---
+++ pricing-page/zendesk/module/color-change.js
@@ -0,0 +1,6 @@
+const changeColorBtn = document.getElementById('color-change')
+const chatbox = document.getElementById('chat-box-content')
+
+changeColorBtn.addEventListener('click', () => {
+    body.appendChild()
+})
